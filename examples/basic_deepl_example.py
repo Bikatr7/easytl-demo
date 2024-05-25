e72e2e033f043e07cd9748ad784ab8a89f686086
Bikatr7 <Bikatr7@proton.me>
2024-05-25 17:13:19 -0600

    Added basic deepl example

---
+++ examples/basic_deepl_example.py
@@ -0,0 +1,13 @@
+from easytl import EasyTL
+
+text_to_translate = "I'm pretty sure that I'm a human. I'm not a robot. Do you understand me?"
+target_language = "JA"
+
+with open("./deepl.txt", "r") as file:
+    api_key = file.read()
+
+EasyTL.set_credentials(credentials=api_key, api_type="deepl")
+
+result = EasyTL.deepl_translate(text=text_to_translate, target_lang=target_language)
+
+print(result)
