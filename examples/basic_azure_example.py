590540cad274cc72caf2ad817015c6eba7aa2462
Bikatr7 <Bikatr7@proton.me>
2024-06-22 14:37:38 -0600

    added basic azure example

---
+++ examples/basic_azure_example.py
@@ -0,0 +1,19 @@
+from easytl import EasyTL
+from easytl.exceptions import RequestException
+
+text_to_translate = "I'm pretty sure that I'm a human. I'm not a robot. Do you understand me?"
+target_language = "JA"
+
+with open("./azure.txt", "r") as file:
+    api_key = file.read()
+
+EasyTL.set_credentials(credentials=api_key, api_type="azure")
+
+is_valid, e = EasyTL.test_credentials("azure", "northcentralus")
+
+result = EasyTL.azure_translate(text=text_to_translate, target_lang=target_language, azure_region="northcentralus", azure_endpoint="https://api.cognitive.microsofttranslator.com")
+
+print(result)
+
+## example output:
+## 私は人間だと確信している。ロボットじゃない。私のことがわかりますか？
